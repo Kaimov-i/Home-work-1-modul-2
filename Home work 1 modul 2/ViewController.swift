f8941dbb7c5d1373cffca37259e7d138a9a49c4b
Kaimov-i <117822275+Kaimov-i@users.noreply.github.com>
2024-03-13 19:59:06 +0300

    Initial Commit

---
+++ Home work 1 modul 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 1 modul 2
+//
+//  Created by Ilman on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
